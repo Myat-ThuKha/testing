e658a1c32a6cd99ccb2740f1d9e623b94771cec9
ann <ann@sanuk.ltd>
2025-07-29 13:29:04 +0630

    testing 123

--- database/client.go
+++ database/client.go
@@ -15,7 +15,7 @@ type DB struct {
 	Name   string
 }
 
-var MONGO_URI = "mongodb://localhost:27017"
+var MONGO_URI = "mongodb://localhost:27017/test?replicaSet=rs0"
 
 func ConnectMongo() (*DB, error) {
 	monitor := &event.CommandMonitor{
